9253b6ccb87eb132bf3239463c577b4ef85d7e6a
zhongyang.wu <zhongyang.wu@outlook.com>
2019-01-26 02:55:54 -0500

    demo

--- chrome-extsion/frame.js
+++ chrome-extsion/frame.js
@@ -1,3 +1,21 @@
-function jump(){
-    location.url =
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('#close').addEventListener('click', close);
+    main();
+});
+
+
+function close() {
+    document.getElementsByTagName("html")[0].setAttribute("style", "display:none;");
+}
+
+function main() {
+    var selectedText = null;
+    var url = window.location.search.substring(1); //get rid of "?" in querystring
+    var qArray = url.split('&'); //get key-value pairs
+    for (var i = 0; i < qArray.length; i++) {
+        var pArr = qArray[i].split('='); //split key and value
+        if (pArr[0] === "text")
+            selectedText = pArr[1];
+    }
+    document.getElementById("text").textContent = selectedText;
 }
